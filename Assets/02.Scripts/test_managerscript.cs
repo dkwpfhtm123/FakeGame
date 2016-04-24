673d599c156245b1535356bae1e34d9ab63f61b1
fake <fake@c8595e04-5401-0010-92cd-69e82cbe71df>
2016-04-24 18:07:31 +0000

    쇠사슬탄막

---
+++ Assets/02.Scripts/test_managerscript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test_managerscript : MonoBehaviour
+{
+    public static test_managerscript Instance = null;
+
+    public bool oncollision;
+    public bool waitTime;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        oncollision = false;
+        waitTime = false;
+    }
+}
--- Assets/02.Scripts/test_movescript.cs
+++ Assets/02.Scripts/test_movescript.cs
@@ -5,49 +5,116 @@ public class test_movescript : MonoBehaviour
 {
     public Vector2 Direction;
     public float BulletSpeed;
+    public bool MakingBullet;
+    public float Angle;
 
-    Transform transformCache;
-
-    float currentTime;
-    Vector2 startingPosition;
-
-    bool firstFrame;
+    private Transform transformCache;
+    private Vector2 startingPosition;
+    private bool creating;
 
     void Start()
     {
-        firstFrame = true;
         transformCache = GetComponent<Transform>();
-        currentTime = 0;
         startingPosition = transformCache.localPosition;
-    }
+        creating = false;
 
-    public float angle;
-    public float dx;
-    public float dy;
+        RotateBullet();
+    }
 
     void Update()
     {
-        if (firstFrame == false)
+        if (test_managerscript.Instance.oncollision == false)
         {
-            dx = 100 * currentTime * Mathf.Deg2Rad;
-            dy = Mathf.Sin(dx);
-
-            Vector2 direction = new Vector2(dx, dy);
-
-            Vector2 targetDirection = direction;
-
-            float Angle = angle * Mathf.Deg2Rad;
-
-            direction.x = targetDirection.x * Mathf.Cos(Angle) - targetDirection.y * Mathf.Sin(Angle);
-            direction.y = targetDirection.x * Mathf.Sin(Angle) + targetDirection.y * Mathf.Cos(Angle);
-
-            BulletSpeed = 5.0f;
-
-            transform.localPosition = startingPosition + (BulletSpeed * direction.normalized * currentTime);
-
-            currentTime += Time.deltaTime;
+            MoveBullet();
+        }
+        else
+        {
+            if (MakingBullet == false)
+            {
+                if (creating == false)
+                {
+                    StartCoroutine(CreateBullet());
+                }
+            }
         }
 
-        firstFrame = false;
+        if (MakingBullet == true)
+        {
+            MoveBullet();
+        }
+    }
+
+    void MoveBullet()
+    {
+        if (transformCache == null)
+        {
+            transformCache = GetComponent<Transform>();
+        }
+
+        Vector2 bulletPosition = transformCache.localPosition;
+
+        var position = transformCache.localPosition;
+        position.x += Direction.x * BulletSpeed * Time.deltaTime;
+        position.y += Direction.y * BulletSpeed * Time.deltaTime;
+
+        transformCache.localPosition = position;
+
+        //       transformCache.localPosition = position;
+        //       transform.localPosition = startingPosition + (BulletSpeed * Direction.normalized * currentTime);
+
+        //    currentTime += Time.deltaTime;
+    }
+
+    void RotateBullet()
+    {
+        Vector2 targetDirection = Direction;
+        float anglePlus = Angle * Mathf.Deg2Rad;
+
+        Direction.x = targetDirection.x * Mathf.Cos(anglePlus) - targetDirection.y * Mathf.Sin(anglePlus);
+        Direction.y = targetDirection.x * Mathf.Sin(anglePlus) + targetDirection.y * Mathf.Cos(anglePlus);
+    }
+
+    IEnumerator CreateBullet()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            test_managerscript.Instance.waitTime = true;
+            creating = true;
+
+            GameObject bullet = Instantiate(gameObject);
+            Transform bulletTransform = bullet.GetComponent<Transform>();
+            test_movescript bulletScript = bullet.GetComponent<test_movescript>();
+
+            bulletTransform.localPosition = transformCache.localPosition;
+            bulletTransform.localRotation = Quaternion.identity;
+            bulletTransform.localScale = Vector3.one * 0.5f;
+
+            bulletScript.BulletSpeed = 3.0f;
+            bulletScript.Angle = Random.Range(0, 360.0f);
+            bulletScript.Direction = Direction;
+            bulletScript.MakingBullet = true;
+
+            yield return new WaitForSeconds(1.0f);
+
+            creating = false;
+        }
+        Angle = Random.Range(0, 360);
+        RotateBullet();
+
+        test_managerscript.Instance.oncollision = false;
+        yield return new WaitForSeconds(2.0f);
+        test_managerscript.Instance.waitTime = false;
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (gameObject.GetComponent<NoWallDestroy>())
+        {
+            if (coll.gameObject.GetComponent<Wall>() == true)
+            {
+                test_managerscript.Instance.oncollision = true;
+                Destroy(gameObject);
+            }
+        }
     }
 }
